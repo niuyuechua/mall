58f3fa06e1f3432030c30957e0cffbd75af9a62c
niuyuechua <13954042673@163.com>
2019-04-19 04:10:15 +0000

    upd

--- app/Http/Controllers/Weixin/WxPayController.php
+++ app/Http/Controllers/Weixin/WxPayController.php
@@ -73,193 +73,93 @@ class WxPayController extends Controller
     }
 
     private  function postXmlCurl($xml, $url, $useCert = false, $second = 30)
-
     {
-
         $ch = curl_init();
-
         //设置超时
-
         curl_setopt($ch, CURLOPT_TIMEOUT, $second);
-
         curl_setopt($ch,CURLOPT_URL, $url);
-
         curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
-
         curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
-
         //设置header
-
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
         //要求结果为字符串且输出到屏幕上
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
 //		if($useCert == true){
-
 //			//设置证书
-
 //			//使用证书：cert 与 key 分别属于两个.pem文件
-
 //			curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
-
 //			curl_setopt($ch,CURLOPT_SSLCERT, WxPayConfig::SSLCERT_PATH);
-
 //			curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
-
 //			curl_setopt($ch,CURLOPT_SSLKEY, WxPayConfig::SSLKEY_PATH);
-
 //		}
-
         //post提交方式
-
         curl_setopt($ch, CURLOPT_POST, TRUE);
-
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-
         //运行curl
-
         $data = curl_exec($ch);
-
         //返回结果
-
         if($data){
-
             curl_close($ch);
-
             return $data;
-
         } else {
-
             $error = curl_errno($ch);
-
             curl_close($ch);
-
             die("curl出错，错误码:$error");
-
         }
-
     }
-
-
-
-
 
     public function SetSign()
-
     {
-
         $sign = $this->MakeSign();
-
         $this->values['sign'] = $sign;
-
         return $sign;
-
     }
-
-
-
-
 
     private function MakeSign()
-
     {
-
         //签名步骤一：按字典序排序参数
-
         ksort($this->values);
-
         $string = $this->ToUrlParams();
-
         //签名步骤二：在string后加入KEY
-
         $string = $string . "&key=".env('WEIXIN_MCH_KEY');
-
         //签名步骤三：MD5加密
-
         $string = md5($string);
-
         //签名步骤四：所有字符转为大写
-
         $result = strtoupper($string);
-
         return $result;
-
     }
 
-
-
     /**
-
      * 格式化参数格式化成url参数
-
      */
-
     protected function ToUrlParams()
-
     {
-
         $buff = "";
-
         foreach ($this->values as $k => $v)
-
         {
-
             if($k != "sign" && $v != "" && !is_array($v)){
-
                 $buff .= $k . "=" . $v . "&";
-
             }
-
         }
-
         $buff = trim($buff, "&");
-
         return $buff;
-
     }
 
-
-
-
-
     /**
-
      * 微信支付回调
-
      */
-
     public function notice()
-
     {
-
         $data = file_get_contents("php://input");
-
-
-
         //记录日志
-
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-
         file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
-
-
-
         $xml = simplexml_load_string($data);
-
-
-
         if($xml->result_code=='SUCCESS' && $xml->return_code=='SUCCESS'){      //微信支付成功回调
-
             //验证签名
-
             $sign = true;
-
-
             if($sign){       //签名验证成功
                 //TODO 逻辑处理  订单状态更新
-
             }else{
                 //TODO 验签失败
                 echo '验签失败，IP: '.$_SERVER['REMOTE_ADDR'];
@@ -267,7 +167,6 @@ class WxPayController extends Controller
             }
         }
         $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
-
         echo $response;
     }
 }
