1b60757e1ee829800beae132ad27e672470ae19e
niuyuechua <13954042673@163.com>
2019-04-24 05:37:40 +0000

    upd

--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -83,17 +83,11 @@
         <div class="title m-b-md">
 
         </div>
-        <img src="http://1809niuyuechyuang.comcto.com/goodsimg/{{$goods['goods_img']}}" alt="">
-        名称：{{$goods['goods_name']}}
+        <img src="http://1809niuyuechyuang.comcto.com/goodsimg/{{$goods['goods_img']}}" alt=""> <hr>
+        名称：{{$goods['goods_name']}} <br>
         价格：{{$goods['goods_price']}}
         <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+
         </div>
     </div>
 </div>
