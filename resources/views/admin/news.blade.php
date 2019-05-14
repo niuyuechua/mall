8eb1a94c9f4be92f74c66986887246e4d69ea2c1
niuyuechua <13954042673@163.com>
2019-05-14 09:31:30 +0000

    群发upd

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -21,7 +21,7 @@
     @endforeach
 </table> <br>
 <p>请输入要发送的内容:<input type="text" id="text"></p>
-<button id="btn">发送</button>    <span class="color:red" id="message"></span>
+<button id="btn">发送</button>
 </body>
 </html>
 <script type="text/javascript" src="/js/jquery/jquery-1.12.4.min.js"></script>
@@ -64,10 +64,8 @@
             success:function(res){
                 if(res=='success'){
                     alert("群发成功");
-                    $("#message").html("群发成功");
                 }else{
                     alert("群发失败");
-                    $("#message").html("群发失败");
                 }
             }
         })
