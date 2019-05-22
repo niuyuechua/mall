90dc747d32789ea5ac47cc28149160fcdb1b70d7
niuyuechua <13954042673@163.com>
2019-05-22 02:21:28 +0000

    upd

--- app/Http/Controllers/Weixin/LoginController.php
+++ app/Http/Controllers/Weixin/LoginController.php
@@ -15,7 +15,7 @@ class LoginController extends Controller
         $openid=session('openid');
         //dump($openid);die;
         if(empty($openid)){
-            echo 1;
+            echo '授权';
             $redirect_url=urlEncode("http://www.nyc666666.top/login/getOpenid");
             $url2="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WX_APP_ID')."&redirect_uri=".$redirect_url."&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
             header("location:$url2");
@@ -28,7 +28,6 @@ class LoginController extends Controller
         $url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APP_ID')."&secret=".env('WX_APP_SEC')."&code=".$code."&grant_type=authorization_code";
         $res=json_decode(file_get_contents($url),true);
         $openid=$res['openid'];
-        dump($openid);die;
         session(['openid'=>$openid]);
         redirect('login/bind');
     }
